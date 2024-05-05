0602db8d794338dc6f5dbc4e2ef5660d29863e10
MehfoozurRehman <mehfoozijaz786@gmail.com>
2024-05-05 14:13:31 +0500

    Update index.ts

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,15 +1,5 @@
-import SvgLogo from './logo.svg'
-import SvgLogoar from './logoar.svg'
-import SvgLogoarwhite from './logoarwhite.svg'
-import SvgLogodark from './logodark.svg'
-import PngLogoicon from './logoicon.png'
-import PngMenulogo from './Menulogo.png'
+import WebpLogo from './logo.webp'
 
 export {
-  SvgLogo,
-  SvgLogoar,
-  SvgLogoarwhite,
-  SvgLogodark,
-  PngLogoicon,
-  PngMenulogo
+  WebpLogo
 }
